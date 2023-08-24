d0a45b6fce28f74dc33f473c87a3378911f7ecf7
kim <kimngan.ledang@zattoo.com>
2023-08-24 18:08:33 +0200

    error page

--- routes/errorRouter.js
+++ routes/errorRouter.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import httpStatus from 'http-status-codes';
 
 const errorRouter = express.Router();
 
 //The 404 Route (ALWAYS Keep this as the last route)
 errorRouter.get('*', function(req, res){
-    res.status(404).render('error', {
+    let errorCode = httpStatus.NOT_FOUND;
+    let errorMessage = "Wir entschuldigen uns! Die Seite, auf die Sie zugreifen wollten, konnte nicht gefunden werden."
+    res.status(errorCode);
+    res.render('error', {
+        errorCode: errorCode,
+        errorMessage: errorMessage,
         title: 'Error | EEE. Karsten Diekmann',
     });
 });
+
 
 export default errorRouter;
